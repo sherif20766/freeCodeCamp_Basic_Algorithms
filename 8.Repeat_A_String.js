1586295ea337734a1d844bf64c728064295d03c2
sherif20766 <sherif20766@hotmail.com>
2017-02-11 17:18:25 +0000

    algorithm 8 completed

---
+++ 8.Repeat_A_String.js
@@ -0,0 +1,13 @@
+//Repeat a given string (first argument) num times (second argument). 
+//Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  return num>0?str.repeat(num):""; 
+}
+
+repeatStringNumTimes("abc", 3);
+
+
+//ES6 Code
+
+const repeatStringNumTimes=(str, num)=>num>0?str.repeat(num):"";
